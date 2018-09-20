884ccbc05baf29387e4cff369aee4dfcad42fc4e
ibrahim <ibrahimibrahim408@gmail.com>
2018-09-20 12:18:13 -0700

    ibrahim first commit

--- App.js
+++ App.js
@@ -24,6 +24,7 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Text>Real nigga back end</Text>
       </View>
     );
   }
