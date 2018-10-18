d8c43b45ea29a43ea9c8406f10384c8b85b4d0e9
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-10-18 00:13:13 -0700

    Style landing page

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,25 +1,51 @@
 import React from "react";
 import { View, StatusBar, Image, SafeAreaView } from "react-native";
-import {Button, Text} from "native-base";
+import { Button, Text } from "native-base";
 
 export default class Landing extends React.Component {
   render() {
     return (
       <SafeAreaView style={{ flex: 1, backgroundColor: "#2F3A49" }}>
         <StatusBar translucent barStyle="light-content" />
-          <Image
-            source={require("../../img/logo.png")}
-            style={{ width: 150, height: 150, overflow: "visible" }}
-            alignSelf='center'
-          />
-          <View>
-              <Text>Welcome to Smart Inventory</Text>
-              <Text>The best way to keep track of all your items. Let's get started</Text>
-          </View>
-          <Text>Continue with:</Text>
+        <Image
+          source={require("../../img/logo.png")}
+          style={{
+            width: 150,
+            height: 150,
+            overflow: "visible",
+            marginTop: 35,
+            marginBottom: 15
+          }}
+          alignSelf="center"
+        />
+        <View
+          style={{
+            marginTop: 20,
+            marginBottom: 20,
+            marginLeft: 15,
+            marginRight: 1,
+            alignContent: "center"
+          }}
+        >
+          <Text style={{ color: "#FFFFFF", fontSize: 40, textAlign: "center" }}>
+            Welcome to Smart Inventory
+          </Text>
+          <Text style={{ color: "#FFFFFF", fontSize: 14, textAlign: "center" }}>
+            The best way to keep track of all your items. Let's get started
+          </Text>
+        </View>
+        <Text style={{ textAlign: "center", color: "#FFFFFF", fontSize: 12 }}>
+          Continue with:
+        </Text>
+        <View style={{
+            marginLeft: 15,
+            marginRight: 15,
+            marginTop: 10
+        }}>
           <Button block>
-              <Text>EMAIL</Text>
+            <Text>EMAIL</Text>
           </Button>
+        </View>
       </SafeAreaView>
     );
   }
