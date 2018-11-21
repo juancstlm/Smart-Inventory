d4796b730ef6bd2459907d4a742c52ae28fe16c4
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-11-20 23:43:39 -0800

    Revert "Merge remote-tracking branch 'origin/join-create'"
    This reverts commit 82799591eed8817942cc198dd976feef6b3cdfaa.

--- src/components/pages/InventoriesList.js
+++ src/components/pages/InventoriesList.js
@@ -2,7 +2,7 @@
 import React from "react";
 import InventoryProfile from "../ui/InventoryProfile";
 import InventoryCardSection from "../ui/InventoryCardSection";
-import { Header, SearchBar, Avatar, ButtonGroup } from "react-native-elements";
+import { Header, SearchBar, Avatar } from "react-native-elements";
 import Firebase from "../../Firebase";
 import {
   Text,
@@ -12,7 +12,6 @@ import {
   TouchableOpacity
 } from "react-native";
 import Modal from "react-native-modal";
-import ActionButton from 'react-native-action-button';
 import Join from "../ui/Join";
 import Create from "../ui/Create";
 
@@ -24,22 +23,9 @@ export default class InventoriesList extends React.Component {
     isJoin: true,
     joinBackColor: "#2f3a49",
     createBackColor: "",
-    search: "",
-    selectedIndex: 0
+    search: ""
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedIndex: 0
-    }
-
-    this.updateIndex = this.updateIndex.bind(this)
-  }
-
-  updateIndex (selectedIndex) {
-    this.setState({selectedIndex})
-  }
   // This is so that react navigator hides the stack header
   static navigationOptions = {
     header: null
@@ -81,6 +67,17 @@ export default class InventoriesList extends React.Component {
 
   _toggleModal = () =>
     this.setState({ isModalVisible: !this.state.isModalVisible });
+
+  join = () => {
+    this.setState({ isJoin: true });
+    this.setState({ joinBackColor: "" });
+    this.setState({ createBackColor: "#2f3a49" });
+  };
+  create = () => {
+    this.setState({ isJoin: false });
+    this.setState({ createBackColor: "" });
+    this.setState({ joinBackColor: "#2f3a49" });
+  };
 
   renderSearchBar() {
 
@@ -130,11 +127,7 @@ export default class InventoriesList extends React.Component {
     }
   }
 
-
   render() {
-    const buttons = ['JOIN', 'CREATE'];
-    const { selectedIndex } = this.state;
-
     return (
 
       <View style={{ flex: 1, backgroundColor: "#2f3a49" }}>
@@ -167,57 +160,80 @@ export default class InventoriesList extends React.Component {
             height: "50%",
             marginTop: 20,
             marginLeft: 0,
-            flex: 1,
+            flex: 1
           }}
           supportedOrientations={["portrait", "landscape"]}
-          onBackdropPress={() => this.setState({ isVisible: false })}>
+          onBackdropPress={() => this.setState({ isVisible: false })}
+        >
+          <View style={{ width: "100%", height: "40%", marginTop: 0 }}>
+            <TouchableOpacity
+              style={{
+                opacity: 0.1,
+                backgroundColor: "white",
+                width: "100%",
+                height: "100%"
+              }}
+              onPress={this._toggleModal}
+            />
+          </View>
 
-            <View style={{ width: "100%", height: "40%", marginTop: 0 }}>
-              <TouchableOpacity
-                style={{
-                  opacity: 0.1,
-                  backgroundColor: "white",
-                  width: "100%",
-                  height: "100%"
-                }}
-                onPress={this._toggleModal}/>
-            </View>
-
+          <View
+            style={{
+              flex: 1,
+              width: "100%",
+              height: "60%",
+              backgroundColor: "#2f3a49",
+              alignItems: "center"
+            }}
+          >
             <View
               style={{
-                flex: 2,
+                height: 3,
                 width: "100%",
-                height: "60%",
-                backgroundColor: "#2f3a49",
-                alignItems: "center"
-              }}>
-              <View
+                backgroundColor: "#f7931e",
+                marginBottom: 3
+              }}
+            />
+            <InventoryCardSection>
+              <TouchableOpacity
+                onPress={this.join}
                 style={{
-                  height: 3,
-                  width: "100%",
-                  backgroundColor: "#f7931e",
-                  marginBottom: 3
-                }}/>
+                  backgroundColor: this.state.joinBackColor,
+                  width: "25%",
+                  justifyContent: "center",
+                  marginLeft: 20,
+                  marginRight: 20
+                }}
+              >
+                <Text style={{ color: "white", fontSize: 20 }}>Join</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={this.create}
+                style={{
+                  backgroundColor: this.state.createBackColor,
+                  width: "25%",
+                  justifyContent: "center",
+                  marginLeft: 20,
+                  marginRight: 20
+                }}
+              >
+                <Text style={{ color: "white", fontSize: 20 }}>Create</Text>
+              </TouchableOpacity>
+            </InventoryCardSection>
+            <TextInput
+              style={{
+                color: "white",
+                height: 40,
+                width: "80%",
+                backgroundColor: "#8190a5",
+                borderWidth: 1
+              }}
+              value={"Quick Share Code"}
+            />
 
-                <ButtonGroup
-                onPress={this.updateIndex}
-                selectedIndex={selectedIndex}
-                buttons={buttons}
-                containerStyle={{height: 35, width: "60%", borderWidth: 0, marginTop: 5}}
-                innerBorderStyle={{color: '#e79100'}}
-                selectedBackgroundColor={"#e79627"}
-                selectedTextStyle={{color: '#fff'}}
-                selectedButtonStyle={{backgroundColor: '#e79627'}}
-                buttonStyle={{backgroundColor:"#2f3a49", borderWidth: 0}}
-                textStyle={{color:'#fff'}} />
-              
-                {this.state.selectedIndex === 0 ? <Join/> : <Create />}
-                          
-            </View>
+            {this.state.isJoin ? <Join /> : <Create />}
+          </View>
         </Modal>
-        <ActionButton buttonColor="rgba(231,76,60,1)" onPress={this._toggleModal}>
-                    
-        </ActionButton>
       </View>
     );
   }
--- src/components/ui/Create.js
+++ src/components/ui/Create.js
@@ -1,84 +1,18 @@
 import React from 'react';
-import {Text,TextInput, View} from 'react-native';
-import {Button, ButtonGroup, Avatar, SearchBar} from 'react-native-elements';
-import InventoryCardSection from "../ui/InventoryCardSection";
+import {Text, View, Image} from 'react-native';
 
-class Create extends React.Component {
 
-    state={name: '', disableCreate: true, selectedIndex: 0}
+const Create = () => {
 
-    constructor(props) {
-        super(props);
-    
-        this.updateIndex = this.updateIndex.bind(this)
-      }
-    
-    create = () => {
-        if (this.state.name === ''){
-            console.log('name not set')
-        }
-        else{
-           //create an inventory object and add user as owner
-           
-        }
+    callParent = () => {
+        //props.callbackFromParent(props.inventory);
     }
 
-    updateIndex (selectedIndex) {
-        this.setState({selectedIndex});
-    }
+    return (
+        <View style={{flex: 1,width:'100%', backgroundColor: 'white'}}>
 
-    render(){
-        const buttons = ['Shared', 'Private'];
-        const { selectedIndex } = this.state;
-
-        return (
-            <View style={{flex: 1, height: '100%',width:'100%', backgroundColor: '#2f3a49', alignItems: 'center'}}>
-                <TextInput
-                        onChangeText={(text) => {this.setState({name: text});
-                        this.setState({ disableCreate: (this.state.name.length < 2) });}} 
-                        clearButtonMode='while-editing'
-                        autoFocus={true}
-                        style={{
-                        color: "white",
-                        height: 40,
-                        fontSize: 18,
-                        width: "80%",
-                        backgroundColor: "#8190a5",
-                        borderWidth: 1,
-                        borderRadius: 3,
-                        marginBottom: 15,
-                        marginTop: 10,
-                        }}
-                        placeholder={" Inventory name"} 
-                        placeholderTextColor={"white"}/>
-
-                <Text style={{color: "white", height: 22,fontSize: 18, alignSelf: 'auto', marginBottom: 1}}>
-                    Sharing:
-                </Text>  
-
-                <ButtonGroup
-                onPress={this.updateIndex}
-                selectedIndex={selectedIndex}
-                buttons={buttons}
-                containerStyle={{height: 35, width: "60%", borderWidth: 0, marginTop: 2}}
-                innerBorderStyle={{color: '#e79100'}}
-                selectedBackgroundColor={"#e79627"}
-                selectedTextStyle={{color: '#fff'}}
-                selectedButtonStyle={{backgroundColor: '#e79627'}}
-                buttonStyle={{backgroundColor:"#2f3a49", borderWidth: 0}}
-                textStyle={{color:'#fff'}} />
-                
-                <InventoryCardSection>
-                     <View style={{flex: 2, height: 100, backgroundColor: '00000000', blurRadius: 1}}/>
-                </InventoryCardSection>
-                
-                <View style={{width: '40%', alignItems:'stretch'}}>
-                    <Button onPress={this.create} title='CREATE' disabled={this.state.disableCreate}/>
-                </View>
-                
-            </View >
-        );
-    }    
+        </View>
+    );
 
 };
 
